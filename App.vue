a01fae8f0fcd1b818195eb12a8cebbefb4d8343a
zy <1473926822@qq.com>
2021-05-08 19:58:31 +0800

    hello 1026

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <p>java1026 全体同学 行走的offer收割机</p>
+      <p>java1026 全体同学 前程似锦</p>
+      <p>java1026 全体同学 666</p>
+      <p>java1026 全体同学 争取第一周入职 第二周转正,第三周升为组长,第四周董事会</p>
+      <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3101694723,748884042&fm=26&gp=0.jpg"/>
+      <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=316865976,2940650346&fm=26&gp=0.jpg"/>
+      <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=316865976,2940650346&fm=26&gp=0.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
